0cf73f0492cb58ba9499392ab7b00773e4d91904
rlarudgk <flyfalls@naver.com>
2022-08-06 02:22:53 +0900

    4분면 알고리즘 문제 풀이

---
+++ src/algorithm/boj/BOJ14681.java
@@ -0,0 +1,11 @@
+package algorithm.boj;
+
+import java.util.Scanner;
+
+public class BOJ14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
